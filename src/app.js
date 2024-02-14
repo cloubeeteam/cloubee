78c60870701a43c054595a869e036ddfc3a14e49
abhisn15 <abhisuryanu9roho@gmail.com>
2024-02-14 22:44:50 +0700

    test

--- src/app.js
+++ src/app.js
@@ -132,15 +132,3 @@ List3.addEventListener("click", () => {
 List4.addEventListener("click", () => {
 	Menu.classList.toggle("animasi");
 });
-
-const root = document.documentElement;
-const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue(
-	"--marquee-elements-displayed",
-);
-const marqueeContent = document.querySelector("ul.marquee-content");
-
-root.style.setProperty("--marquee-elements", marqueeContent.children.length);
-
-for (let i = 0; i < marqueeElementsDisplayed; i++) {
-	marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
-}
